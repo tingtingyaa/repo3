fcbf07d87f64b055536a72d2d3251316018cd71e
zhai123 <2290252138@qq.com>
2019-10-29 17:38:36 +0800

    repo3

--- src/test/java/CeshiTest.java
+++ src/test/java/CeshiTest.java
@@ -2,5 +2,8 @@ public class CeshiTest {
     public static void main(String[] args) {
 
         System.out.println("afdmmmmdknkjkfd");
+        System.out.println("afdmmmmdknkjkfd");
+        System.out.println("afdmmmmdknkjkfd");
+        System.out.println("afdmmmmdknkjkfd");
     }
 }
