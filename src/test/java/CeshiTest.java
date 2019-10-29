e725414837a77a84788afb2ef649c241bdeaf3c7
tingtingya <1787697908@qq.com>
2019-10-29 17:21:16 +0800

    11

--- src/test/java/CeshiTest.java
+++ src/test/java/CeshiTest.java
@@ -1,7 +1,6 @@
 public class CeshiTest {
     public static void main(String[] args) {
-        System.out.println("kkzkk");
-        System.out.println("hsj");
+
         System.out.println("wwwwwww");
     }
 }
