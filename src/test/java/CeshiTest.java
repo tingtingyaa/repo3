d9db3ce0c13b8e70bd4679993bc4031b6bb7d947
tingtingya <1787697908@qq.com>
2019-10-29 18:14:00 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test/java/CeshiTest.java

--- src/test/java/CeshiTest.java
+++ src/test/java/CeshiTest.java
@@ -1,12 +1,7 @@
 public class CeshiTest {
     public static void main(String[] args) {
 
-        System.out.println("afdmmmmdknkjkfd");
-        System.out.println("afdmmmmdknkjkfd");
-        System.out.println("afdmmmmdknkjkfd");
-        System.out.println("afdmmmmdknkjkfd");
-        System.out.println("xfadsf");
-        System.out.println("bbbbbbbbbb");
+        System.out.println("........");
 
     }
 }
