20fff78814eb3daf390798e3f6b62420e53f5bc1
tingtingya <1787697908@qq.com>
2019-10-29 17:08:28 +0800

    Merge branch 'master' of https://github.com/tingtingyaa/repo3
    # Conflicts:
    #	.idea/repo3.iml

--- src/test/java/CeshiTest.java
+++ src/test/java/CeshiTest.java
@@ -1,7 +1,5 @@
 public class CeshiTest {
     public static void main(String[] args) {
-        System.out.println("yayayayyaya");
-        System.out.println("hahahhahahahahah");
-        System.out.println("hahahhahahahahah");
+
     }
 }
