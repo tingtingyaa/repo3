806d31a1b410668122d9e34be5dbf5d86a416fba
zhai123 <2290252138@qq.com>
2019-10-29 17:34:25 +0800

    repo3

--- src/test/java/CeshiTest.java
+++ src/test/java/CeshiTest.java
@@ -1,7 +1,6 @@
 public class CeshiTest {
     public static void main(String[] args) {
 
-        System.out.println("iiiiiiii");
-        System.out.println("tttt");
+        System.out.println("afd");
     }
 }
